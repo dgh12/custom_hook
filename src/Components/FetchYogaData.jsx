c15b6c0da4b8dbee79fbd94f67a6c7f3adbfb429
DGH12 <dghedding@gmail.com>
2024-08-28 16:07:05 -0600

    saving

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -10,9 +10,8 @@ function FetchYogaData() {
           <>
             <li className='list_data'>
               <h3>{e.name}</h3>
-              <p><strong>Importance: </strong>{e.importance}</p>
-              <p><strong>Benefits: </strong>{e.benefits6}</p>
-              <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+              <p><strong>Benefits: </strong>{e.benefits}</p>
+              <p><strong>Time to do: </strong>{e.time_duration}</p>
             </li>
           </>
       ))}
